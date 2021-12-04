ac5229ab4295a3c9cb6c27bdc9e9856e0595831e
Savio Santos <saviosa08@gmail.com>
2021-12-04 14:25:56 -0300

    Removed api post affirmative actions.

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,26 +1,8 @@
 import {
-    Button,
     Navbar
 } from "react-bootstrap"
-import { useState } from "react"
-import { getData } from "../services/filter-sheet"
-import axios from "axios"
 
 export function Menu() {
-    const [sheet, setSheet] = useState();
-
-    const onLoadSheet = (event) => {
-        let file = event.target.files[0];
-        setSheet(file);
-    }
-
-    const onAddSheet = async () => {
-        if (sheet) {
-            let data = await getData(sheet);
-            await axios.post("/api/actions", data);
-            window.location.reload();
-        }
-    }
 
     return (
         <Navbar className="small-padding bg-blue d-flex justify-content-between">
@@ -30,13 +12,3 @@ export function Menu() {
         </Navbar>
     )
 }
-
-/*
-{!sheet &&
-                <input onChange={onLoadSheet} type="file" placeholder="Carregar planilha"></input>
-            }
-            {sheet &&
-                <Button className="button" onClick={onAddSheet}>
-                    Registrar respostas
-                </Button>}
-                */
